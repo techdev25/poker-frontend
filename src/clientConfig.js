d7d40393127e457205623855abc4c636f3eba0f9
techdev25 <t.ichikawa911@gmail.com>
2025-03-08 14:08:48 +0900

    fix

--- src/clientConfig.js
+++ src/clientConfig.js
@@ -1,12 +1,9 @@
 const config = {
-  isProduction: process.env.NODE_ENV === 'production',
-  contentfulSpaceId: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-  contentfulAccessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
-  googleAnalyticsTrackingId: process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID,
-  socketURI:
-    process.env.NODE_ENV === 'production'
-      ? process.env.REACT_APP_SERVER_URI
-      : `http://${window.location.hostname}:5000/`,
+  isProduction: true,
+  contentfulSpaceId: "4tlsutsg5se1",
+  contentfulAccessToken: "_BZqCnlzI0RuHvLPcOIfNxjUfS082jSJ9WmevnXSLdk",
+  googleAnalyticsTrackingId: "",
+  socketURI: "https://ed64-118-8-82-157.ngrok-free.app/",
 };
 
 export default config;
